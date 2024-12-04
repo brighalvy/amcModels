3d75f8b876b5665ba75529f1df21a0989c631ac7
Brigham Halverson <brighalvy@tamu.edu>
2024-12-04 14:03:10 -0600

    Wrote amc function.

--- R/amc.R
+++ R/amc.R
@@ -6,7 +6,7 @@
 #'
 #' @param N A two or three dimensional array (KxIxJ) where K is the number of subgroups, I is the number of transitory states, J is the total number of states.
 #' @param B Number of posterior draws to get (default is 10000)
-#' @param xsi the value to be included as the prior parameter in the uniform dirichlet prior for each row.
+#' @param xsi the a vector of length I where xsi is the prior value for the uniform dirichlet prior.
 #'
 #' @return A three dimensional array
 #' @export
@@ -16,10 +16,41 @@
 
 amc <- function(N, B = 10000, xsi){
   # Check compatability of N:
+  d <- dim(N)
   if (!is.array(N)) {
     stop(paste("N must be an array."))
   }
-  if (length(dim(N)) < 3) {
-    stop(paste("N must be a three dimensional array."))
+  if (length(d) < 2) {
+    stop(paste("N must be a two orthree dimensional array."))
   }
+  if(length(d) == 3){
+    N_new <- array(NA, dim = c(d[2], d[3]))
+    for(i in 1:d[2]){
+      N_new[i, ] <- colSums(N[ , i, ], na.rm = TRUE)
+    }
+    N <- N_new
+  }
+
+  I <- nrow(N)
+  J <- ncol(J)
+
+  if (!is.null(xsi)) {
+    if(xsi <= 0){
+      stop(paste("xsi must be positive"))
+    }
+  }
+  if(is.null(xsi)){
+    xsi <- c()
+    for(i in 1:I){
+      xsi[i] <- 1/sum(!is.na(N[i, ]))
+    }
+  }
+
+  ## Get posterior draws:
+  theta <- array(0, dim = c(B, I, J))
+  for(i in 1:I){
+    theta[ , I, !is.na(N[i, ])] <- LaplacesDemon::rdirichlet(B, N[i, !is.na(N[i, ])] + rep(xsi[i], sum(!is.na(N[i, ]))))
+  }
+
+  return(theta)
 }
