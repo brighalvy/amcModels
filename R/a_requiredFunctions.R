27f76bd62e1d972e3c98b70dcd713dc3003b169f
Brigham Halverson <brighalvy@tamu.edu>
2026-03-25 20:17:53 -0500

    correct sequential update.

--- R/a_requiredFunctions.R
+++ R/a_requiredFunctions.R
@@ -347,8 +347,8 @@ update_groupings_seq <- function(n_i,
       if(j != length(ids + 1)){
         log_lik[j] <- sum(n_i[i, ] * log(theta[j,]))
       } else {
-        theta_use <- LaplacesDemon::rdirichlet(1, alpha*gamma + n_i[i,])
-        log_lik[j] <- sum((n_i[i, ] + alpha*gamma) * log(theta_use[1,]))
+        theta_use <- LaplacesDemon::rdirichlet(1, alpha*gamma)
+        log_lik[j] <- sum((n_i[i, ]) * log(theta_use[1,]))
       }
       log_prior[j] <- log_epa_prior(p_use, beta, delta, dist, sigma)
     }
