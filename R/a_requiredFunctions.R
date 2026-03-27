6cfe231b8440754105b960c8683488ab0690836a
Brigham Halverson <brighalvy@tamu.edu>
2026-03-27 11:02:20 -0500

    trial

--- R/a_requiredFunctions.R
+++ R/a_requiredFunctions.R
@@ -347,7 +347,7 @@ update_groupings_seq <- function(n_i,
       if(j != length(ids + 1)){
         log_lik[j] <- sum((n_i[i, ]) * log(theta[j,])) # + exp(alpha+gamma)
       } else {
-        theta_use <- LaplacesDemon::rdirichlet(1, exp(alpha+gamma))
+        theta_use <- LaplacesDemon::rdirichlet(1, n_i[i, ] + exp(alpha+gamma))
         log_lik[j] <- sum((n_i[i, ] + exp(alpha+gamma)) * log(theta_use[1,]))
       }
       log_prior[j] <- log_epa_prior(p_use, beta, delta, dist, sigma)
